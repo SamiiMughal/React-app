8b1284f467a2d8ab71e0aabd200ba95cbd3cd0bf
SamiiMughal <samiaslam680@gmail.com>
2024-01-02 21:54:18 +0500

    Add Component Folder with header.js file

---
+++ src/component/header.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+<div>
+    <div className='nav'
+    ></div>
+    <div className='body'
+    ></div>
+    <div className='footer'
+    ></div>
+</div>
+    
+}
