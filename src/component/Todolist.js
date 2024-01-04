98e50d8c974107a5cd42995952eb27e1543cb622
SamiiMughal <samiaslam680@gmail.com>
2024-01-04 23:52:51 +0500

    working todo list

--- src/component/Todolist.js
+++ src/component/Todolist.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import App from './App';
 
-export const TodoItem = ({ todo, toggleTodo, deleteTodo }) => {
+export const Todoitem = ({ todo, toggleTodo, deleteTodo }) => {
   return (
     <li className={todo.completed ? 'completed' : ''}>
       <span onClick={() => toggleTodo(todo.id)}>{todo.text}</span>
       <button onClick={() => deleteTodo(todo.id)}>Delete</button>
     </li>
   );
-};export default Todoitem;
+};
+export default Todoitem;
 
